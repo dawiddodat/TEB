3e44dd31f66791a570163cd5102a83c59a53fe19
dawiddodat <47181048+dawiddodat@users.noreply.github.com>
2019-03-29 14:07:13 +0100

    DVszv

---
+++ wyrazenia3.js
@@ -0,0 +1,9 @@
+elHaslo = document.getElementById('haslo');
+elHaslo2 = document.getElementById('haslo2');
+
+elDlugosc = document.getElementById('dlugosc');
+elCyfra = document.getElementById('cyfra');
+elDuzalitera = document.getElementById('duzalitera');
+elMalalitera = document.getElementById('malalitera');
+elZnak = document.getElementById('znak');
+elKomunikat = document.getElementById('komunikat');
